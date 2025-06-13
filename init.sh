b40416f289e8fcb2390fe2db362b432065fda7d0
cmacwill1 <charles.macwilliams@gmail.com>
2025-06-13 10:03:24 -0400

    starter scripts

---
+++ init.sh
@@ -0,0 +1,15 @@
+!#/bin/bash
+if test -e $HOME/.bashrc; then
+  if grep -q "export PATH=$HOME/helpful-scripts/bin:$PATH" $HOME/.bashrc; then
+    echo "helpful-scripts are already on the PATH"
+  else
+    echo "export PATH=$HOME/helpful-scripts/bin:$PATH" >> $HOME/.bashrc
+    source $HOME/.bashrc
+    echo "helpful-scripts have been added to the PATH!"
+  fi
+else
+  touch $HOME/.bashrc
+  echo "export PATH=$HOME/helpful-scripts/bin:$PATH" >> $HOME/.bashrc
+  source $HOME/.bashrc
+  echo ".bashrc created, and helpful-scripts have been added to the PATH!"
+fi
